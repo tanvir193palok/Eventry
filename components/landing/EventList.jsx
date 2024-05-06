952a0d4b8aec72fe23bc4b9aa29a2a185c388bbd
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-05-06 23:25:33 +0600

    Hosted mongoDB database in mongoDb atlas

--- components/landing/EventList.jsx
+++ components/landing/EventList.jsx
@@ -4,6 +4,8 @@ import EventCard from "./EventCard";
 const EventList = async ({ query }) => {
   const allEvents = await getAllEvents(query);
 
+  console.log(allEvents)
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
       {allEvents.map((event) => (
--- services/mongo.js
+++ services/mongo.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export async function dbConnect() {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "eventry",
+    });
     console.log("Connected");
     return connection;
   } catch (error) {
